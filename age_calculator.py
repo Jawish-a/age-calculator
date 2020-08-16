3ec491fb0a99ba63df51c6cef70248d2ba8d7d67
jawish-a@outlook.com <jawish-a@outlook.com>
2020-08-16 14:08:27 +0300

    first commit

--- age_calculator.py
+++ age_calculator.py
@@ -2,12 +2,28 @@ from datetime import datetime
 
 def check_birthdate(year, month, day):
 	# write code here
+	birthdate = datetime(year, month, day)
+	if birthdate > datetime.now():
+		return False
+	else:
+		return True
+	
 
 def calculate_age(year, month, day):
     # write code here
+	now = datetime.now()
+	age = now.year - year
+	print("You are " + str(age) + " years old.")
 
 def main():
 	# write main code here
+	year = int(input("Enter year of birth: "))
+	month = int(input("Enter month of birth: "))
+	day = int(input("Enter day of birth: "))
+	if check_birthdate(year, month, day):
+		calculate_age(year, month, day)
+	else:
+		print("the birthdate is invalid.")
 
 
 if __name__ == '__main__':
